02da814cfd1f8688d93169681a47eb82d1d5a6f5
Yonela <yoza.futho@gmail.com>
2023-07-04 15:53:03 +0200

    Still working on the cart

---
+++ main/app.js
@@ -0,0 +1,32 @@
+function pizzaCost(){
+
+    
+}
+
+
+
+// const url = `https://pizza-api.projectcodex.net/api/pizzas`;
+
+// async function getPizzaCart() {
+
+    
+
+//     axios.get(url).then((result) => {
+//         const pizzas = result.data.pizzas;
+        
+//         // this.pizzas is declared on you AlpineJS Widget.
+//         this.pizzas = pizzas;
+//     })
+    
+//     axios.post(url, { data : 'here' }).then((result) => {
+//         const pizzas = result.data.pizzas;
+        
+//         // this.pizzas is declared on you AlpineJS Widget.
+//         this.pizzas = pizzas;
+//     })
+    
+      
+// }
+
+// // Calling the function
+// getPizzaCart();
